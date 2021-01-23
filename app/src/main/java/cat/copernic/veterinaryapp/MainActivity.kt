bfe65b75016dad43c4e56bdd602cdd6950748e9b
joseceuve <71776803+joseceuve@users.noreply.github.com>
2021-01-23 15:17:06 +0100

    Añadido binding, esqueleto login. Añadido view a main sobre escrito onclick, Toast de prueba para analizar

--- app/src/main/java/cat/copernic/veterinaryapp/MainActivity.kt
+++ app/src/main/java/cat/copernic/veterinaryapp/MainActivity.kt
@@ -2,17 +2,36 @@ package cat.copernic.veterinaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import cat.copernic.veterinaryapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(
-                
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            R.layout.activity_main)
+        //Botones
+        binding.btnLogIn.setOnClickListener(this)
     }
-    //Comrpueba que los datos del correo y contraseña no estan vacíos
-    fun analizarDatosUsuario(){
+    //Comprueba que los datos del correo y contraseña no estan vacíos
+    private fun analizarDatosUsuario(){
+        //TODO
+    }
+     private fun login(email: String, password: String){
+         //TODO
+         //Tost temporal pra probar
+         Toast.makeText(this,"Pulasado LogIn", Toast.LENGTH_LONG).show()
+     }
+    override fun onClick(v: View) {
+        when(v.id) {
+            //Llamada a metodos de los botones
+            R.id.btnLogIn -> login("tmp user","tmp pass")
 
+        }
     }
 }
