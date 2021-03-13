eedaaf58e52038ee4f87a402c3dcbca2454d6f08
joseceuve <71776803+joseceuve@users.noreply.github.com>
2021-03-13 11:28:50 +0100

    Ya se guarda en la db la modificación de mascota.

--- app/src/main/java/cat/copernic/veterinaryapp/Fragment_Editar_Mascota.kt
+++ app/src/main/java/cat/copernic/veterinaryapp/Fragment_Editar_Mascota.kt
@@ -1,16 +1,20 @@
 package cat.copernic.veterinaryapp
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import cat.copernic.veterinaryapp.dataBase.OperaciondBDfirebase_mascota
 
 import cat.copernic.veterinaryapp.databinding.FragmentEditarMascotaBinding
 import cat.copernic.veterinaryapp.modelos.Mascota
 import com.google.firebase.firestore.FirebaseFirestore
 import cat.copernic.veterinaryapp.Fragment_Editar_MascotaArgs
+import cat.copernic.veterinaryapp.modelos.Perfil
 
 
 /**
@@ -41,6 +45,12 @@ class Fragment_Editar_Mascota : Fragment() {
         var chip = args.chipMascota
         genui(chip)
 
+        binding.btnModificarMascota.setOnClickListener{
+            Log.e("JoseEDM","Click")
+            guardaDatos()
+        }
+
+
     }
 
     /**
@@ -55,5 +65,65 @@ class Fragment_Editar_Mascota : Fragment() {
             binding.editTextNumberSigned.setText(it.get("pes") as String?)
             binding.editTextRaza.setText(it.get("rassa") as String?)
         }
+    }
+
+    private fun guardaDatos(){
+        var mascotaMod = Mascota()
+        val comprueba = Comprobaciones()
+        var campoErroneo = false
+
+        var nombre = binding.nombreMascota.text.toString()
+        var fechaNac = binding.editTextFechaNacMascota.text.toString()
+        //El chip no se modifica
+        var peso = binding.editTextNumberSigned.text.toString()
+        var raza = binding.editTextRaza.text.toString()
+        var chip = binding.chipMascota.text.toString()
+
+
+
+        if(!comprueba.contieneTexto(nombre)){
+            campoErroneo = true
+        }else if(!comprueba.validaFecha(fechaNac)){
+            campoErroneo = true
+        } else if (!comprueba.contieneTexto(peso)){ //Por modificar, peso valido
+            campoErroneo = true
+        } else if (!comprueba.contieneTexto(raza)){
+            campoErroneo = true
+        }else{
+            //No hay error llenar datos de la clase
+            mascotaMod.nom = nombre
+            mascotaMod.pes = peso
+            mascotaMod.dataNaixement = fechaNac
+            mascotaMod.rassa = raza
+            mascotaMod.numCgip = chip
+
+            Log.e("JoseEDM", mascotaMod.numCgip.toString())
+            Log.e("JoseEDM", mascotaMod.nom.toString())
+            Log.e("JoseEDM", mascotaMod.pes)
+            Log.e("JoseEDM", mascotaMod.dataNaixement.toString())
+            Log.e("JoseEDM", mascotaMod.rassa.toString())
+            val opdb: OperaciondBDfirebase_mascota = OperaciondBDfirebase_mascota()
+            //Guardar las modificaciones
+            opdb.modificar(mascotaMod)
+        }
+        //Si hay algun campo erroneo
+        if (campoErroneo){
+            missatgeEmergent("Error","Omple tots els camps correctament")
+            campoErroneo = false //No es necesario, pero por si acaso
+        }
+    }
+
+    /**
+     * Mensaje emergente
+     * Pasar titulo y mensaje a mostrar
+     */
+    fun missatgeEmergent(titol: String, missatge: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(titol)
+        builder.setMessage(missatge)
+        builder.setPositiveButton("Aceptar") { dialog, which ->
+        }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
--- app/src/main/java/cat/copernic/veterinaryapp/dataBase/OperaciondBDfirebase_mascota.kt
+++ app/src/main/java/cat/copernic/veterinaryapp/dataBase/OperaciondBDfirebase_mascota.kt
@@ -1,5 +1,6 @@
 package cat.copernic.veterinaryapp.dataBase
 
+import android.util.Log
 import cat.copernic.veterinaryapp.modelos.Mascota
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -26,7 +27,19 @@ class OperaciondBDfirebase_mascota: OperacionsBDMascota {
     }
 
     override fun modificar(mascota: Mascota): Boolean {
-        TODO("Not yet implemented")
+        Log.e("JoseDB", mascota.dataNaixement.toString())
+        Log.e("JoseDB", mascota.nom.toString())
+        Log.e("JoseDB", mascota.pes.toString())
+        Log.e("JoseDB", mascota.rassa.toString())
+        db.collection("Mascota").document(mascota.numCgip).update(
+            mapOf(
+                "nom" to mascota.nom.toString(),
+                "pes" to mascota.pes.toString(),
+                "rassa" to mascota.rassa.toString(),
+                "data_naixement" to  mascota.dataNaixement.toString()
+            )
+        )
+        return true //De momento meramente decorativo, pero lo pide la interfaz, por si hay que capturar exceptions.    }
     }
 
     override fun buscar(mascota: Mascota): Boolean {
