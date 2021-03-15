4b18d42593b1d437f0c1b5b40150a35472efbe13
joseceuve <71776803+joseceuve@users.noreply.github.com>
2021-03-15 19:50:52 +0100

    preparando generar diagnostico

--- app/src/main/java/cat/copernic/veterinaryapp/vet_generardiag.kt
+++ app/src/main/java/cat/copernic/veterinaryapp/vet_generardiag.kt
@@ -10,11 +10,14 @@ import cat.copernic.veterinaryapp.dataBase.OperacionesDBFirebase_Diagnostico
 import cat.copernic.veterinaryapp.databinding.FragmentVetGenerardiagBinding
 import cat.copernic.veterinaryapp.modelos.Diagnostico
 
+
 /**
  * Fragmento generar diagnostico
  *
  */
 class vet_generardiag : Fragment() {
+    private lateinit var binding: FragmentVetGenerardiagBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,37 +27,11 @@ class vet_generardiag : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        //Hago el binding
-        var binding = inflate<FragmentVetGenerardiagBinding>(
-            inflater,
-            R.layout.fragment_vet_generarcita,
-            container,
-            false
-        )
-        //var diagnostico = Diagnostico(null, "", "xxx","Asesinato","Pastis",null,"Pepito") //Muestra llenar datos
-
-        //De momento sin contenido, se lo paso a la vista del xml
-        var diagnostico = Diagnostico()
-        //binding.diagnostico = diagnostico
-
-        //Click boton enviar
-        binding.btnEnviar.setOnClickListener {
-
-            //diagnostico.fecha //Si es de hoy generar fecha
-            diagnostico.diagnostico = binding.editTextDiagnostico.text.toString()
-            diagnostico.medicamento = binding.editTextMedicamento.text.toString()
-            //diagnostico.veterinario //Obtener nombre veterinario
-            //diagnostico.prox_visita = binding.diagnostico.prox_visita
-            //diagnostico.paciente //Buscar el funcionamiento del campo.
-
-
-            var opdiag = OperacionesDBFirebase_Diagnostico()
-            opdiag.guardar(diagnostico) //Guardo los datos en la db, lo mismo que modificar si los campos estan llenos por el que se hace la busqueda.
-        }
+        binding = FragmentVetGenerardiagBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
 
 }
+
+
